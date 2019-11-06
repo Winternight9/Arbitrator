80682bbb64ea2e87dce7b45b1fbbb782c46d181d
KornSiwat <siwat@siwat.dev>
2019-11-06 11:36:40 +0700

    allowed user to create account then redirect to page with success message

--- arbitrator/views/registrationView.py
+++ arbitrator/views/registrationView.py
@@ -1,11 +1,30 @@
 import sys
 
 from django.shortcuts import render, reverse, redirect
+from django.contrib.auth import authenticate, login
 from ..forms import RegistrationForm
+from django.contrib import messages
 
 
 def registrationView(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+
+            user = authenticate(username=username, password=raw_password)
+
+            login(request, user)
+
+            messages.success(request, f"Account: {username} created.\nWelcome to Arbitrator")
+
+            return redirect(reverse("arbitrator:test"))
+    else:
+        form = RegistrationForm()
+
     context = {
         'form':  form
     }
