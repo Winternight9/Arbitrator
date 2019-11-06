d8a939ba720efd77227f1cccc3ee71d8547d2a02
KornSiwat <siwat@siwat.dev>
2019-11-06 15:28:19 +0700

    fixed conflict

--- arbitrator/urls.py
+++ arbitrator/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from django.urls import include
-from .views import indexView, loginView, logoutView, createStatistic, registrationView,test
+from .views import indexView, loginView, logoutView, createStatistic, registrationView, test
 
 
 app_name = 'arbitrator'
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', indexView, name='index'),
     path('login/', loginView, name='login'),
     path('register/', registrationView, name='registration'),
-    path('stat/',createStatistic, name='login'),
-    path('logout/',logoutView, name='logout'),
+    # path('stat/',createStatistic, name='login'),
+    path('logout/', logoutView, name='logout'),
     path('test/', test, name='test'),
 ]
