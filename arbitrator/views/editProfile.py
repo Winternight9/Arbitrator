809b804cdec0f644694b5ae1f84382ebfc2fcb74
Winternight9 <poomiix9@gmail.com>
2019-11-13 14:03:37 +0700

    add editprofile feature

--- arbitrator/views/editProfile.py
+++ arbitrator/views/editProfile.py
@@ -1,6 +1,6 @@
 import sys
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,reverse
 from django.contrib.auth.models import User
 from arbitrator.forms import EditProfileForm
 
@@ -10,12 +10,13 @@ def editProfile(request):
 
         if form.is_valid():
             form.save()
-            return redirect('/arbitrator/profile')
 
+            return redirect(reverse("arbitrator:profile"))
     else:
         form = EditProfileForm(instance =request.user)
         args = {'form' : form}
-        return render(request, 'arbitrator/editProfile.html', args)        
 
+        return render(request, 'arbitrator/editProfile.html', args)        
+ 
 
 sys.modules[__name__] = editProfile
