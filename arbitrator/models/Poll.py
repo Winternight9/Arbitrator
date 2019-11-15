11ea6b9e5f562491e2b0f202e8dcdfe0607f601e
Siwat Ponpued <siwatponpued@gmail.com>
2019-11-15 12:00:30 +0700

    restructured and renamed models

---
+++ arbitrator/models/Poll.py
@@ -0,0 +1,16 @@
+import sys
+
+from django.db import models
+from arbitrator.models import ArbitratorUser
+
+
+class Poll(models.Model):
+    owner = models.ForeignKey(ArbitratorUser, on_delete=models.CASCADE)
+    label = models.CharField(max_length=100)
+    created_date = models.DateTimeField('created date', auto_now_add=True)
+
+    def __str__(self):
+        return self.label
+
+
+sys.modules[__name__] = Poll
