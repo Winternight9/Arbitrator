ea0d69630cbef5e96c95b9e07cd07047cedef129
KornSiwat <siwat@siwat.dev>
2019-11-21 12:29:47 +0700

    fixed Arbitrator User pubdate

--- arbitrator/models/ArbitratorUser.py
+++ arbitrator/models/ArbitratorUser.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class ArbitratorUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now=True)
 
     def __str__(self):
         return self.user
