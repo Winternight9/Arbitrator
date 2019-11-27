686de375ce66464a948af6329821a2e58951d3a3
KornSiwat <siwat@siwat.dev>
2019-11-27 18:29:50 +0700

    added css for profile and editProfile

--- arbitrator/views/profileView.py
+++ arbitrator/views/profileView.py
@@ -1,8 +1,10 @@
 import sys
 
 from django.shortcuts import redirect,render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def profileView(request):
     return render(request,'arbitrator/profile.html')
 
