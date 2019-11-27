d035bcfb38397ff5b12ca2d0bb72575713aa25aa
BenZacs <pep52414@gmail.com>
2019-11-28 00:06:48 +0700

    fixed test cases for profile and editProfile by splitting testcases for the case of before login and after logged in

--- arbitrator/tests/test_views.py
+++ arbitrator/tests/test_views.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.shortcuts import render,reverse
 
 
 class ViewsTest(TestCase):
     def setUp(self):
+        self.client = Client()
         self.username = "testuser"
         self.userpass = "123$*HCfjdksla"
         self.user = User.objects.create_user(self.username,password=self.userpass)
-
 
     def test_index(self):
         c = self.client
@@ -43,15 +43,33 @@ class ViewsTest(TestCase):
         response = c.get(path='/home/')
         self.assertTemplateUsed(response, 'arbitrator/home.html')
 
-    def test_profile(self):
+    def test_profile_not_logged_in(self):
         c = self.client
         response = c.get(path='/profile/')
+        status = response.status_code
+        self.assertEqual(status, 302)
+
+    def test_edit_profile_not_logged_in(self):
+        c = self.client
+        response = self.client.get(path='/profile/edit')
+        status = response.status_code
+        self.assertEqual(status, 301)
+
+    def test_profile_logged_in(self):
+        self.client.force_login(self.user)
+        response = self.client.get(path='/profile/')
+        status = response.status_code
+        self.assertEqual(status, 200)
+
+    def test_edit_profile_logged_in(self):
+        self.client.force_login(self.user)
+        response = self.client.get(path='/profile/edit/')
         status = response.status_code
         self.assertEqual(status, 200)
 
     def test_profile_template(self):
-        c = self.client
-        response = c.get(path='/profile/')
+        self.client.force_login(self.user)
+        response = self.client.get(path='/profile/')
         self.assertTemplateUsed(response, 'arbitrator/profile.html')
 
     def test_user_can_login(self):
