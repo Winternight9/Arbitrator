0e6e7cddbc2a18a65910b6c8c26ddeb5206ac3d0
BenZacs <pep52414@gmail.com>
2019-12-03 13:59:01 +0700

    create PollForm

--- arbitrator/forms/PollForm.py
+++ arbitrator/forms/PollForm.py
@@ -4,13 +4,56 @@ import sys
 from django import forms
 from django.utils.safestring import mark_safe
 from arbitrator.models import Poll
+from arbitrator.models import Question
+from arbitrator.models import QuestionType
 
 
 class PollForm(forms.Form):
     def __init__(self, poll_id, *args, **kwargs):
         super(PollForm, self).__init__(*args, **kwargs)
         self.label_suffix = ""
-        self.poll =  Poll.objects.get(id=poll_id)
-        self.questions = self.poll.questions_set.values()
+        self.poll = Poll.objects.get(id=poll_id)
+        self.questions = Question.objects.filter(poll_id=poll_id)
+        for question in self.questions:
+            question_type = question.type
 
-sys.module[__name__] = PollForm
+            if question_type == QuestionType.Text.value:
+                self.fields[question.label] = forms.CharField(
+                    min_length=1,
+                    widget=forms.TextInput(
+                        attrs={
+                            'class': 'input',
+                        }
+                    ),
+                    label=mark_safe(f"{question.label}</br>")
+                )
+            elif question_type == QuestionType.Choice.value:
+                choices = question.choice_set.values()
+                CHOICES = [
+                    (str(choice["id"]), str(choice["label"]))
+                    for choice in choices
+                ]
+
+                multiple_selection = question.multiple_selection
+
+                if multiple_selection:
+                    self.fields[question.label] = forms.MultipleChoiceField(
+                        widget=forms.CheckboxSelectMultiple(
+                            attrs={
+                                'class': 'checkbox',
+                            }
+                        ),
+                        choices=CHOICES
+                    )
+                else:
+                    self.fields[question.label] = forms.ChoiceField(
+                        widget=forms.ChoiceField(
+                            attrs={
+                                'class': 'checkbox',
+                            }
+                        ),
+                        choices=CHOICES
+                    )
+
+
+sys.modules[__name__] = PollForm
--- arbitrator/urls.py
+++ arbitrator/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
 from django.urls import include
-from .views import indexView, loginView, logoutView, registrationView, homeView, profileView, editProfileView, createPollView
+from arbitrator.views import indexView
+from arbitrator.views import loginView
+from arbitrator.views import logoutView
+from arbitrator.views import registrationView
+from arbitrator.views import homeView
+from arbitrator.views import profileView
+from arbitrator.views import editProfileView
+from arbitrator.views import createPollView
+from arbitrator.views import pollView
 from arbitrator.auth import createArbitratorUserFromGoogleAccount
 
 
@@ -16,9 +24,10 @@ urlpatterns = [
     path('profile/', profileView, name='profile'),
     path('profile/edit/', editProfileView, name='editProfile'),
     path(
-        'createArbitratorUserFromGoogleAccount',
+        'createArbitratorUserFromGoogleAccount/',
         createArbitratorUserFromGoogleAccount,
         name='createArbitratorUserFromGoogleAccount'
     ),
-    path('createPoll', createPollView, name='createPoll')
+    path('createPoll/', createPollView, name='createPoll'),
+    path('poll/<int:poll_id>/', pollView, name='poll')
 ]
