f3970f35a826f5e3df0ba84bcd9361919f734773
Winternight9 <poomiix9@gmail.com>
2019-12-07 22:50:23 +0700

    fix static root

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = (
     BASE_DIR + '/arbitrator/static/' ,
