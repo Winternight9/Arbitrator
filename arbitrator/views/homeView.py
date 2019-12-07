3624ddda545be23a093705a02c0ff72b4fd448da
KornSiwat <siwat@siwat.dev>
2019-12-07 21:43:51 +0700

    implemented sort poll by vote availability

--- arbitrator/views/homeView.py
+++ arbitrator/views/homeView.py
@@ -2,12 +2,22 @@ import sys
 
 from django.shortcuts import render, reverse, redirect
 from arbitrator.models import Poll
+from arbitrator.models import PollSubmission
 
 
 def home(request):
-    polls = Poll.objects.all()
+    polls = Poll.objects.all().order_by('-is_vote_available', '-pollsubmission',)
+
+    voted_poll_ids = [
+        submission.poll_id
+        for submission in PollSubmission.objects.filter(
+            user_id=request.user.id
+        )
+    ]
+
     context = {
-        "polls": polls
+        "polls": polls,
+        "voted_poll_ids": voted_poll_ids
     }
 
     return render(request, 'arbitrator/home.html', context)
