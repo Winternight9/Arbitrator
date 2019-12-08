de17992fbe5b245f4f6f17d2cc78d83a042359fb
Winternight9 <poomiix9@gmail.com>
2019-12-08 10:50:46 +0700

    add whitenoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root='/static/files')
+application.add_files('/arbitrator/static/files', prefix='more-files/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
