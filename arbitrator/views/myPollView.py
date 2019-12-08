8fd0fad46b2d1d4282826dcc25395d5a11768a28
KornSiwat <siwat@siwat.dev>
2019-12-08 16:51:07 +0700

    fixed bugs

--- arbitrator/views/myPollView.py
+++ arbitrator/views/myPollView.py
@@ -3,11 +3,12 @@ import sys
 from django.shortcuts import render, reverse, redirect
 from arbitrator.models import Poll
 from django.contrib.auth.decorators import login_required
+from arbitrator.models import ArbitratorUser
 
 
 @login_required
 def myPollView(request):
-    polls = Poll.objects.filter(owner_id=request.user.id)
+    polls = Poll.objects.filter(owner_id=ArbitratorUser.objects.get(user_id=request.user.id).id)
     context = {
         "polls": polls,
     }
